2476fe7c5d337aa62da58a17f4bd55b8926f04ff
WFS-Entrepreneurship-Website <71910763+ReedGraff@users.noreply.github.com>
2022-04-09 07:58:05 -0400

    proxy work

--- proxy.php
+++ proxy.php
@@ -6,10 +6,10 @@
     $opts = array(
         'http'=>array(
         'method'=>"GET",
-        'header' => "Connection: keep-alive" .
-                    "Cache-Control: max-age=0" .
-                    "Sec-Ch-Ua-Mobile: ?0" .
-                    "Sec-Ch-Ua-Platform: 'Windows'"
+        'header' => "Connection: keep-alive;" .
+                    "Cache-Control: max-age=0;" .
+                    "Sec-Ch-Ua-Mobile: ?0;" .
+                    "Sec-Ch-Ua-Platform: 'Windows;'"
         )
     );
     
@@ -57,4 +57,46 @@ Accept-Encoding: gzip, deflate, br
 Accept-Language: en-US,en;q=0.9,es;q=0.8
 
 Cookie: cookieconsent_status=dismiss; language=en; welcomebanner_status=dismiss; continueCode=kvvQWDpj5xnrN9VlbRg2eyZL0e9iZqfgnUNr06OEkzMPYo8amJXBKw4173qZ; _xsrf=2|a3418cb1|2ec5d3b41af1b5af7c836df08df52b4b|1648787265; username-localhost-8888="2|1:0|10:1648787560|23:username-localhost-8888|44:ZTcxOWNiNjVhM2ZmNGY5ODkxMTNlYTE1YWMzYzQ5NDk=|1c6b8aa79900dd3313a348b947e67d3ec33de3898275ea4254e6f5cccf816823"
+-->
+
+<!--
+Total-Route-Time: 0
+
+X-Request-Start: 1649432465057
+
+Connect-Time: 0
+
+Via: 1.1 vegur
+
+X-Forwarded-Port: 443
+
+X-Forwarded-Proto: https
+
+X-Forwarded-For: 50.202.55.2
+
+X-Request-Id: 4b7965b1-7a62-46c9-bcd8-3fd339997b9e
+
+Accept-Language: en-US,en;q=0.9,es;q=0.8
+
+Accept-Encoding: gzip, deflate, br
+
+Sec-Fetch-Dest: document
+
+Sec-Fetch-User: ?1
+
+Sec-Fetch-Mode: navigate
+
+Sec-Fetch-Site: none
+
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36
+
+Upgrade-Insecure-Requests: 1
+
+Sec-Ch-Ua-Platform: "Windows"
+
+Sec-Ch-Ua-Mobile: ?0
+
+Sec-Ch-Ua: " Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"
 -->
